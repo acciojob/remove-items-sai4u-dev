87a2fa444d414564806624b6fa8097c5c2febb09
Sai Narendra <tannirusainarendra81@gmail.com>
2025-08-09 18:40:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-//your JS code here. If required.
+ const colorSelect = document.getElementById('colorSelect');
+  const removeBtn = document.getElementById('removeBtn');
+
+  removeBtn.addEventListener('click', () => {
+    // Check if something is selected
+    if (colorSelect.selectedIndex >= 0) {
+      // Remove the selected option
+      colorSelect.remove(colorSelect.selectedIndex);
+    }
+  });
